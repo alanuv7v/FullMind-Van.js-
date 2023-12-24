30dbb3761f5433b7dda23e454cd628210cd64ab8
alanuv7v <>
2023-12-24 08:19:08 +0000

    sdafdsaf

--- src/script.js
+++ src/script.js
@@ -35,125 +35,163 @@ let testObj = {
 
 
 
-
 let initTargets = {
   'MultilineTextarea' : []
 }
 
 const InOutInterface = (path=[], head, iteration, keyType) => {
-  //if ('name' in data) data.name = 'asdf'; console.log(data, head); return
-  //proved that the data property is referencing prop in head. But still input elems cannot change head's prop.
-  if (iteration < 1) {
-    return
-  }
-  if (!head) {
-    return
-  }
+    //if ('name' in data) data.name = 'asdf'; console.log(data, head); return
+    //proved that the data property is referencing prop in head. But still input elems cannot change head's prop.
+    if (iteration < 1) {
+        return
+    }
+    if (!head) {
+        return
+    }
 
-  let key = path[path.length-1]
-  let data = nestedObj(head, path)
-  console.log(key, data)
+    let resizeTargets = []
 
-  let prevKey = key
-  
-  let key_
+    let key = path[path.length-1]
+    let data = nestedObj(head, path)
+    console.log(key, data)
 
-  if (keyType==="Array") {
-    key_ = MultilineTextarea(
-      textarea({style: "color: rgb(61 210 227)"}, key),
-      textarea({style: "color: rgb(61 210 227)"}, key)
-    )
-  } else {
-    key_ = MultilineTextarea(
-      textarea({style: "color: rgb(80 215 154)", oninput: (event) => {
-          let originalValue = (nestedObj(head, path))
-          nestedObj(head, path.slice(0, -1), 
-              {...nestedObj(head, path.slice(0, -1))/* siblings */,
-              [event.target.value]: originalValue}
-          )
-          path = [...path.slice(0, -1), event.target.value]
-          if (prevKey) nestedObj(head, [...path.slice(0, -1), prevKey], null, "delete")
-          prevKey = event.target.value
-          console.log(nestedObj(head, path), head, prevKey)
-      }}, key),
-      textarea({style: "color: rgb(80 215 154)"}, key)
-    )
-  }
+    let prevKey = key
+    
+    let key_
 
-  initTargets['MultilineTextarea'].push(key_)
-
-  //!!!!!!!! textarea에서 set할 때도 head에서부터 nestedObj 함수로 안에 있는 값을 바꿔야 한다. Path가 너무 길어지는 비효율의 발생은 Dictionary를 만들고, 사전에 고유명사가 등록되면 고유명사에 nested된 prop은 고유명사 obj 안에만 들어있게 하자. 아니면 진짜 head를 listify해버리자. 쉬운 레퍼런스, shallow copy 위해.
-  //setting values_
-  let values_ = []
-  if (typeof data != 'object') { // value is not Object, or it is null
-    values_ = MultilineTextarea(
-      textarea({oninput: (event) => {
-        nestedObj(head, path, event.target.value)
-        console.log(nestedObj(head, path), head)
-      }}, data),
-      textarea(data)
-    )
-    initTargets['MultilineTextarea'].push(values_)
-
-  } else { // value is Object
-    if (data == null) {
-        values_ = d({style:"display: flex"},
-            t.select(
-                t.option("null"),
-                t.option("String"),
-                t.option("Number"),
-                t.option("List"),
-                t.option("Object"),
-                t.option("Boolean"),
-                t.option("Image"),
-                t.option("File"),
-                t.option("Else..."),
+    if (keyType==="Array") {
+        key_ = MultilineTextarea(
+        textarea(key),
+        textarea({style: "color: rgb(61 210 227)"}, key)
+        )
+    } else {
+        key_ = MultilineTextarea(
+        textarea({oninput: (event) => {
+            let originalValue = (nestedObj(head, path))
+            nestedObj(head, path.slice(0, -1), 
+                {...nestedObj(head, path.slice(0, -1))/* siblings */,
+                [event.target.value]: originalValue}
             )
+            path = [...path.slice(0, -1), event.target.value]
+            if (prevKey) nestedObj(head, [...path.slice(0, -1), prevKey], null, "delete")
+            prevKey = event.target.value
+            console.log(nestedObj(head, path), head, prevKey)
+        }}, key),
+        textarea({style: "color: rgb(80 215 154)"}, key)
         )
     }
-    let childKeyType
-    if (Array.isArray(data)) childKeyType = "Array"
-    for (let key in data) {
-      values_.push(IOI([...path, key], head, iteration, childKeyType))
+
+    resizeTargets.push(key_)
+
+    //!!!!!!!! textarea에서 set할 때도 head에서부터 nestedObj 함수로 안에 있는 값을 바꿔야 한다. Path가 너무 길어지는 비효율의 발생은 Dictionary를 만들고, 사전에 고유명사가 등록되면 고유명사에 nested된 prop은 고유명사 obj 안에만 들어있게 하자. 아니면 진짜 head를 listify해버리자. 쉬운 레퍼런스, shallow copy 위해.
+    //setting values_
+    let values_ = []
+    if (typeof data != 'object') { // value is not Object, or it is null
+        values_ = MultilineTextarea(
+        textarea({oninput: (event) => {
+            nestedObj(head, path, event.target.value)
+            console.log(nestedObj(head, path), head)
+        }}, data),
+        textarea(data)
+        )
+        resizeTargets.push(values_)
+
+    } else { // value is Object
+        if (data == null) {
+            values_ = d({style:"display: flex"},
+                t.select(
+                    t.option("null"),
+                    t.option("String"),
+                    t.option("Number"),
+                    t.option("List"),
+                    t.option("Object"),
+                    t.option("Boolean"),
+                    t.option("Image"),
+                    t.option("File"),
+                    t.option("Else..."),
+                )
+            )
+        }
+        let childKeyType
+        if (Array.isArray(data)) childKeyType = "Array"
+        for (let key in data) {
+        values_.push(IOI([...path, key], head, iteration, childKeyType))
+        }
     }
-  }
-  iteration--
+    iteration--
 
-  let valueWrapper = d({style: "padding-left: 1em; padding-bottom: 1em; padding-right: 1em;"},
-        values_
-    )
+    let valueWrapper = d({class:"valueWrapper", style: "padding-left: 1em; padding-bottom: 1em; padding-right: 1em;"},
+            values_
+        )
 
-  let foldArrow = d({style: "margin-left: auto"}, "\u25BD")
 
-  function onIOIwheel(event) {
-    
-    if (!event.shiftKey) {return}
+    function resize() {
+        for (let mt of resizeTargets) {
+            resizeTextarea(mt.children[0], mt.children[1])
+        }   
+        console.log(values_)
+        for (let v of values_) {
+            resizeIOI(v)
+        }
+    }
 
-    event.preventDefault()
-    event.stopPropagation()
+    let foldArrow = d({style: "margin-left: auto"}, "\u25BD")
 
-    if (event.deltaY < 0) {//up, fold
+    function fold() {
         valueWrapper.style.display = "none";
         foldArrow.innerText = "\u25C1"
     }
-    else { //open
+    function open() {
         valueWrapper.style.display = "block";
         foldArrow.innerText = "\u25BD"
+        resize()
+        
     }
-  }
 
+    function onIOIwheel(event) {
+        
+        if (!event.shiftKey) {return}
 
+        event.preventDefault()
+        event.stopPropagation()
 
-  return div({class: "main IOI", onwheel: () => onIOIwheel(event)}, 
-    d({style: "display:flex; flex-direction: row"},
-        key_,
-        foldArrow
-    ),
-    valueWrapper
-  )
+        if (event.deltaY < 0) {//up
+            open()
+        }
+        else { 
+            fold()
+        }
+    }
+    fold()
+
+    initTargets["MultilineTextarea"].push(...resizeTargets)
+
+    let main = div({class: "main IOI", onwheel: () => onIOIwheel(event)}, 
+        d({style: "display:flex; flex-direction: row"},
+            key_,
+            foldArrow
+        ),
+        valueWrapper
+    )
+
+    return main
 }
 
 const IOI = InOutInterface
+
+
+
+
+function resizeIOI(target) {
+    let children = Array.from(target.children)
+    let resizeTargets = [...children.filter((c) => {return c.classList.contains("MultilineTextarea")}), 
+                        ...Array.from(children.find((c) => {return c.classList.contains("valueWrapper")}).children).filter((c) => {return c.classList.contains("MultilineTextarea")})
+                        ]
+    console.log(resizeTargets)
+    for (let mt of resizeTargets) {
+        resizeTextarea(mt.children[0], mt.children[1])
+    }   
+}
 
 
 
@@ -280,10 +318,13 @@ const App = (head) => {
   
     global.ContextMenu = ContextMenu()
   
-    return div({id: 'App'},
-      div({innerText: "Diver", class:"main"}),
-      div({id: "upperMenu"},
-        button("heads...")
+    return div({id: 'App', /* style: "display: flex; flex-direction: row; " */},
+      div({id: "header", style: "display: flex; flex-direction: row; "},
+        button({style: "flex-grow: 1;"}, "head name"),
+        button("depth"),
+        button("account"),
+        button("settings"),
+        button({style: "margin-left: auto"}, "Diver version 0.1")
       ),
       div({id: "view", class:"main"},
           seed,
