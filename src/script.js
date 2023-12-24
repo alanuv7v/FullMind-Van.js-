dd8b5b472ded359bf8063639a847f2a399be8df7
alanuv7v <>
2023-12-24 06:54:06 +0000

    stylin

--- src/script.js
+++ src/script.js
@@ -14,7 +14,7 @@ codeanywhere에서 변경사항 있을 시 커밋 뿐만 아니라 push도 꼭 �
 const log = (text) => console.log(text)
 
 const t = van.tags
-const {div, span, button, textarea} = t
+const {div, span, button, textarea, input} = t
 /* destructuring 만세!! */
 const d = div
 
@@ -40,7 +40,7 @@ let initTargets = {
   'MultilineTextarea' : []
 }
 
-const InOutInterface = (path=[], head, iteration) => {
+const InOutInterface = (path=[], head, iteration, keyType) => {
   //if ('name' in data) data.name = 'asdf'; console.log(data, head); return
   //proved that the data property is referencing prop in head. But still input elems cannot change head's prop.
   if (iteration < 1) {
@@ -55,28 +55,37 @@ const InOutInterface = (path=[], head, iteration) => {
   console.log(key, data)
 
   let prevKey = key
+  
+  let key_
 
-  let key_ = MultilineTextarea(
-    textarea({style: "color: #8adfd8", oninput: (event) => {
-        let originalValue = (nestedObj(head, path))
-        nestedObj(head, path.slice(0, -1), 
-            {...nestedObj(head, path.slice(0, -1))/* siblings */,
-            [event.target.value]: originalValue}
-        )
-        path = [...path.slice(0, -1), event.target.value]
-        if (prevKey) nestedObj(head, [...path.slice(0, -1), prevKey], null, "delete")
-        prevKey = event.target.value
-        console.log(nestedObj(head, path), head, prevKey)
-    }}, key),
-    textarea({style: "color: #8adfd8"}, key)
-  )
+  if (keyType==="Array") {
+    key_ = MultilineTextarea(
+      textarea({style: "color: rgb(61 210 227)"}, key),
+      textarea({style: "color: rgb(61 210 227)"}, key)
+    )
+  } else {
+    key_ = MultilineTextarea(
+      textarea({style: "color: rgb(80 215 154)", oninput: (event) => {
+          let originalValue = (nestedObj(head, path))
+          nestedObj(head, path.slice(0, -1), 
+              {...nestedObj(head, path.slice(0, -1))/* siblings */,
+              [event.target.value]: originalValue}
+          )
+          path = [...path.slice(0, -1), event.target.value]
+          if (prevKey) nestedObj(head, [...path.slice(0, -1), prevKey], null, "delete")
+          prevKey = event.target.value
+          console.log(nestedObj(head, path), head, prevKey)
+      }}, key),
+      textarea({style: "color: rgb(80 215 154)"}, key)
+    )
+  }
 
   initTargets['MultilineTextarea'].push(key_)
 
   //!!!!!!!! textarea에서 set할 때도 head에서부터 nestedObj 함수로 안에 있는 값을 바꿔야 한다. Path가 너무 길어지는 비효율의 발생은 Dictionary를 만들고, 사전에 고유명사가 등록되면 고유명사에 nested된 prop은 고유명사 obj 안에만 들어있게 하자. 아니면 진짜 head를 listify해버리자. 쉬운 레퍼런스, shallow copy 위해.
   //setting values_
   let values_ = []
-  if (typeof data != 'object' || data == null) { // value is not Object, or it is null
+  if (typeof data != 'object') { // value is not Object, or it is null
     values_ = MultilineTextarea(
       textarea({oninput: (event) => {
         nestedObj(head, path, event.target.value)
@@ -87,8 +96,25 @@ const InOutInterface = (path=[], head, iteration) => {
     initTargets['MultilineTextarea'].push(values_)
 
   } else { // value is Object
+    if (data == null) {
+        values_ = d({style:"display: flex"},
+            t.select(
+                t.option("null"),
+                t.option("String"),
+                t.option("Number"),
+                t.option("List"),
+                t.option("Object"),
+                t.option("Boolean"),
+                t.option("Image"),
+                t.option("File"),
+                t.option("Else..."),
+            )
+        )
+    }
+    let childKeyType
+    if (Array.isArray(data)) childKeyType = "Array"
     for (let key in data) {
-      values_.push(IOI([...path, key], head, iteration))
+      values_.push(IOI([...path, key], head, iteration, childKeyType))
     }
   }
   iteration--
@@ -204,55 +230,6 @@ function init() {
 
   updateContextMenu({fromIndex: 0, toAdd: defaultMenu})
 
-  var sample = {
-    name: "Alice",
-    age: 25,
-    address: {
-      city: "Seoul",
-      country: "South Korea"
-    }
-  };
-  
-  var container = document.getElementById ("container");
-  
-// A function that creates an input element for each property of a nested object
-// obj: the nested object
-// parent: the parent element to append the input elements
-// path: an array of keys that represents the path of the property
-function createInputs (obj, parent, path) {
-  // Loop through each property of the object
-  for (var key in obj) {
-    // If the property is an object, call the function recursively with the new path
-    if (typeof obj[key] === "object") {
-      createInputs (obj[key], parent, path.concat (key));
-    } else {
-      // Otherwise, create an input element and set its value and name attributes
-      var input = document.createElement ("input");
-      input.type = "text";
-      input.value = obj[key];
-      input.name = path.concat (key).join ("."); // The name attribute is the dot-separated path of the property
-      // Add an event listener that updates the object when the input value changes
-      input.addEventListener ("keydown", function (e) {
-        console.log(JSON.stringify(sample, null, 2));
-        // Split the name attribute by dots to get the path of the property
-        var keys = e.target.name.split (".");
-        // Use reduce to access the nested property and assign the new value
-        keys.reduce (function (accumulated, current, i) {
-          // If it is the last key, assign the new value
-          if (i === keys.length - 1) {
-            accumulated[current] = e.target.value;
-          } else {
-            // Otherwise, return the next object
-            return accumulated[current];
-          }
-        }, obj);
-      });
-      // Append the input element to the parent element
-      parent.appendChild (input);
-    }
-  }
-}
-createInputs (sample, container, []);
 
 }
 
@@ -305,7 +282,9 @@ const App = (head) => {
   
     return div({id: 'App'},
       div({innerText: "Diver", class:"main"}),
-      div({id: "container"}),
+      div({id: "upperMenu"},
+        button("heads...")
+      ),
       div({id: "view", class:"main"},
           seed,
       ),
